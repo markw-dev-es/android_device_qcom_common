c21ad46f3936b605bf32a9324f44b14f9b09e4c4
Nikolay Karev <karev.n@gmail.com>
2018-01-09 19:28:17 +0500

    msm8953: dont' disable cores at sleep to fix sticky cores

--- power/power-8953.c
+++ power/power-8953.c
@@ -236,8 +236,8 @@ int set_interactive_override(__unused struct power_module *module, int on)
                int resource_values[] = {
                    TIMER_RATE_BIG, 0x32,
                    //TIMER_RATE_LITTLE, 0x32,
-                   CPUS_ONLINE_MAX_LIMIT_BIG, 0x1,
-                   CPUS_ONLINE_MAX_LIMIT_LITTLE, 0x3,
+                   //CPUS_ONLINE_MAX_LIMIT_BIG, 0x1,
+                   //CPUS_ONLINE_MAX_LIMIT_LITTLE, 0x3,
                    MAX_FREQ_BIG_CORE_0, 0x327,
                    MAX_FREQ_LITTLE_CORE_0, 0x327,
                    THREAD_MIGRATION_SYNC_ON_V3, 0x0,
